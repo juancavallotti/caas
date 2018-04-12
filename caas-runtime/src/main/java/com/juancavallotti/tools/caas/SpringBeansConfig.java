f43ff0e552392353a0a30a9f99d59efc777aeb31
Juan Alberto López Cavallotti <juancavallotti@gmail.com>
2018-04-12 13:20:59 -0700

    Cosmetic improvement on imports

--- caas-runtime/src/main/java/com/juancavallotti/tools/caas/SpringBeansConfig.java
+++ caas-runtime/src/main/java/com/juancavallotti/tools/caas/SpringBeansConfig.java
@@ -6,8 +6,6 @@ import com.juancavallotti.tools.caas.spi.ConfigurationServiceDataProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
@@ -15,8 +13,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.*;
-import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -29,8 +29,7 @@ import java.util.Optional;
 import java.util.ServiceLoader;
 import java.util.stream.Collectors;
 
-
-import static com.juancavallotti.tools.caas.RuntimeProperties.*;
+import static com.juancavallotti.tools.caas.RuntimeProperties.CONFIG_LOCATION;
 
 @Configuration
 @EnableAutoConfiguration
